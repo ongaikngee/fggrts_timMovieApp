9762eea40245f84d38c01d7f6f89dfd6f7d8ebf9
Jonathan Ong <73453714+ongaikngee@users.noreply.github.com>
2026-05-14 16:49:02 +0800

    feat: implement MovieCard component and update App to display movie cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css'
+import "./App.css";
+import MovieCard from "./components/MovieCard";
 
 function App() {
-
   return (
-    <h1>Hello</h1>
-  )
+    <>
+      <MovieCard movie={{ title: "Mandolorian", url:"https://mockmind-api.uifaces.co/content/cartoon/32.jpg", released_date: "2024" }} />
+      <MovieCard movie={{ title: "Mandolorian2", url:"https://mockmind-api.uifaces.co/content/cartoon/22.jpg", released_date: "2024" }} />
+    </>
+  );
 }
 
-export default App
+export default App;
