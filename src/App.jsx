9e60295e9968087e09aebd7081ef93b3599c9bae
Jonathan Ong <73453714+ongaikngee@users.noreply.github.com>
2026-05-15 09:10:45 +0800

    feat: implement routing with react-router-dom and add Favourites page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import Favourites from "./pages/Favourites";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home></Home>
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/fav" element={<Favourites />} />
+      </Routes>
+    </main>
   );
 }
 
