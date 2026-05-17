507ae039d47c69285e09c14962f6d06ef2b85f81
Jonathan Ong <73453714+ongaikngee@users.noreply.github.com>
2026-05-17 16:17:45 +0800

    feat: enhance Favourites component to display favorite movies using context

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import "../css/Favorites.css"
+import React from "react";
+import "../css/Favorites.css";
+import MovieCard from "../components/MovieCard";
+import { useMovieContext } from "../contexts/MovieContext";
 
 const Favourites = () => {
-  return (
-    <div className='favorites-empty'>
-        <h2>No Favorite Movies Yet</h2>
-        <p>Start adding movies to your favorites and they will appear here.</p>
-    </div>
-  )
-}
+  const { fav } = useMovieContext();
 
-export default Favourites
+  if (fav.length > 0) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {fav.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here.</p>
+    </div>
+  );
+};
+
+export default Favourites;
